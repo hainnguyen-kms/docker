2574e9f3ad6f9ab4f0c2d870a66e400a640f27ad
hainnguyen <hainnguyen@kms-technology.com>
2020-02-14 09:32:29 +0700

    test again

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,7 +18,7 @@ public class DemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String ...args) {
 		initTable();
-		System.out.println("done");
+		System.out.println("done szzxzcxzc");
 	}
 
 	private void initTable() {
